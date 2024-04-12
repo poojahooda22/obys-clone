64b7a6b91ab65db23c16133b30998acd4a2a6048
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-12 23:23:21 +0530

    updated index

--- src/components/Navbar/Index.jsx
+++ src/components/Navbar/Index.jsx
@@ -2,9 +2,9 @@
 
 function Navbar() {
   return (
-    <div className="page1 w-full h-screen bg-red-400">
+    <div className="page1 w-full h-screen">
       <h2>Navabr</h2>
-      
+
     </div>
   )
 }
