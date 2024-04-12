fee2ff3c860fc036f1c87acdcfbf4ffdbb2a3c2b
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-12 13:56:54 +0530

    updated index

--- src/components/loader/Index.jsx
+++ src/components/loader/Index.jsx
@@ -1,20 +1,35 @@
 
 
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from '@gsap/react';
+
+gsap.registerPlugin(ScrollTrigger);
+
 function Loader() {
+    useGSAP(() => {
+        const tl = gsap.timeline();
+        tl.from('.line', {
+            y: 100,
+            stagger: 0.5,
+        })
+
+    }) 
+    
   return ( 
     <div>
         <div 
             className="loader w-full h-full bg-[#0b0b0b] fixed z-[9] font-[PlinaReg]
-            py-[20vh] px-[10vw] uppercase leading-[7vw] tracking-tight 
+            py-[20vh] px-[8vw] uppercase leading-[7vw] tracking-tight 
             text-[7vw]"
         >
-            <div className="line">
+            <div className="line h-fit">
                 <h1>Your</h1>
             </div>
-            <div className="line">
+            <div className="line h-fit">
                 <h1>Web Experience</h1>
             </div>
-            <div className="line">
+            <div className="line h-fit">
                 <h1>is loading right</h1>
             </div>
         </div>
