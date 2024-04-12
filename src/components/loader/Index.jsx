288c8e827131524a350f68c65cda0331b0a4709e
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-12 12:39:16 +0530

    updated index

--- src/components/loader/Index.jsx
+++ src/components/loader/Index.jsx
@@ -5,14 +5,14 @@ function Loader() {
     <div>
         <div 
             className="loader w-full h-full bg-[#0b0b0b] fixed z-[9] font-[PlinaReg]
-            py-[20vh] px-[10vw] 
+            py-[20vh] px-[10vw] uppercase leading-[8vw] 
             text-[7vw]"
         >
             <div className="line">
                 <h1>Your</h1>
             </div>
             <div className="line">
-                <h1>Web Design</h1>
+                <h1>Web Experience</h1>
             </div>
             <div className="line">
                 <h1>is loading right</h1>
