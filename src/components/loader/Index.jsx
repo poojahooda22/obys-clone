b02ef6055e507a6da2ab6ecf4414f345340dc909
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-12 13:18:11 +0530

    updated index

--- src/components/loader/Index.jsx
+++ src/components/loader/Index.jsx
@@ -1,34 +1,25 @@
-import {useRef, useEffect} from 'react';
 
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { Power4, } from 'gsap/gsap-core';
-import { useGSAP } from '@gsap/react';
-
-gsap.registerPlugin(ScrollTrigger);
 
 function Loader() {
-
-
-  return (
+  return ( 
     <div>
         <div 
             className="loader w-full h-full bg-[#0b0b0b] fixed z-[9] font-[PlinaReg]
             py-[20vh] px-[10vw] uppercase leading-[7vw] tracking-tight 
             text-[7vw]"
         >
-            <div className="line ">
+            <div className="line">
                 <h1>Your</h1>
             </div>
-            <div className="line ">
+            <div className="line">
                 <h1>Web Experience</h1>
             </div>
-            <div className="line ">
+            <div className="line">
                 <h1>is loading right</h1>
             </div>
         </div>
     </div>
-  )
+   )
 }
 
 export default Loader
