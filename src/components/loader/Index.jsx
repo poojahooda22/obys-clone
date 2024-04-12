2ed65f73f33da99a9292358da3a117b868dc711c
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-12 14:27:01 +0530

    updated

--- src/components/loader/Index.jsx
+++ src/components/loader/Index.jsx
@@ -1,4 +1,4 @@
-
+import styles from './Style.module.css'
 
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
@@ -9,9 +9,10 @@ gsap.registerPlugin(ScrollTrigger);
 function Loader() {
     useGSAP(() => {
         const tl = gsap.timeline();
-        tl.from('.line', {
-            y: 100,
+        tl.from('.line h1', {
+            y: 150,
             stagger: 0.5,
+            duration: 1,
         })
 
     }) 
@@ -20,16 +21,18 @@ function Loader() {
     <div>
         <div 
             className="loader w-full h-full bg-[#0b0b0b] fixed z-[9] font-[PlinaReg]
-            py-[20vh] px-[8vw] uppercase leading-[7vw] tracking-tight 
-            text-[7vw]"
+            
+            px-[5vw] py-[60vw] leading-[7.8vw] text-[7.4vw] tracking-tighter
+            sm:py-[20vh] sm:px-[8vw] uppercase sm:leading-[7vw] sm:tracking-tight
+            sm:text-[7vw] "
         >
-            <div className="line h-fit">
+            <div className={`line ${styles.line}`}>
                 <h1>Your</h1>
             </div>
-            <div className="line h-fit">
+            <div className={`line ${styles.line}`}>
                 <h1>Web Experience</h1>
             </div>
-            <div className="line h-fit">
+            <div className={` line ${styles.line}`}>
                 <h1>is loading right</h1>
             </div>
         </div>
