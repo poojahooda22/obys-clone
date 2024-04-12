36c232589c2a80e21197c9536f11ef7bfa2467b4
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-12 20:59:43 +0530

    updated index

--- src/components/loader/Index.jsx
+++ src/components/loader/Index.jsx
@@ -3,19 +3,28 @@ import styles from './Style.module.css'
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useGSAP } from '@gsap/react';
+import { useEffect } from 'react';
 
 gsap.registerPlugin(ScrollTrigger);
 
 function Loader() {
 
-    setInterval(function() {
-        
-
-    }, 100);
+    useEffect(() => {
+        var h5timer = document.querySelector('.part1 h5');
+        var grow = 0;
+        setInterval(function() {
+            if(grow < 100) {
+                h5timer.innerHTML = grow++;
+            } else {
+                h5timer.innerHTML = grow;
+            }
+        },50);
+    }, []);
+    
 
     useGSAP(() => {
         const tl = gsap.timeline();
-        tl.from('.line h1', {
+        tl.from('.line h1, h2', {
             y: 150,
             stagger: 0.3,
             duration: 1,
@@ -26,36 +35,46 @@ function Loader() {
   return ( 
     <div>
         <div 
-            className="loader w-full h-full bg-[#0b0b0b] fixed z-[9] font-[PlinaReg]
-            px-[5vw] py-[46vw] leading-[7.8vw] text-[7.4vw] tracking-tighter
-            sm:py-[20vh] sm:px-[6vw] uppercase sm:leading-[7vw] sm:tracking-tight sm:text-[7vw]
-            xl:text-[5.8vw] xl:leading-[6vw] xl:py-[30vh]"
+            className="loader w-full h-full bg-[#0b0b0b] fixed z-[9] font-[PlinaReg] 
+            px-[5vw] py-[40vw] leading-[7.8vw] text-[7.4vw] tracking-tighter
+            sm:py-[18vw] sm:px-[6vw]  sm:leading-[7vw] sm:tracking-tight sm:text-[7vw]
+            xl:text-[5.8vw] xl:leading-[6vw] xl:py-[10vw]"
         >
             <div 
-                className={`line ${styles.line} flex flex-col items-start
+                className={`line ${styles.line} flex flex-col items-start uppercase
                 sm:flex-row sm:items-center sm:justify-start gap-[2vw]
                 
                 `}
             >
                 <div 
-                    className={`part1 ${styles.part1} flex items-center justify-center gap-[1.4vw]
-                    font-[silkSerif] text-[4.2vw] leading-[4.5vw] tracking-wide
-                    sm:text-[3vw]`}
+                    className={`part1 ${styles.part1} flex  gap-[2.5vw]
+                    items-center justify-center sm:gap-[1vw]
+                    font-[silkSerif] text-[4.2vw] leading-[4.5vw] tracking-wide sm:items-start
+                    sm:text-[2.4vw]`}
                 >
-                    <h5>00</h5>
+                    <h5 className='w-[5vw] text-right '>00</h5>
                     <h6>- 100</h6>
                 </div>
                 <h1>Your</h1>
             </div>
-            <div className={`line ${styles.line}`}>
+            <div className={`line ${styles.line} uppercase`}>
                 <h1>Web Experience</h1>
             </div>
             <div 
                 className={` line ${styles.line} flex 
-                 sm:items-center sm:justify-start gap-[1.5vw] `}
+                 sm:items-center sm:justify-start gap-[1.5vw] uppercase `}
             >
                 <h1>is loading right</h1>
                 <h2>Now</h2>
+            </div>
+            <div 
+                className={`line ${styles.line} sm:w-[30vw] 
+                text-[4vw] mt-[14vw] tracking-normal
+                xl:text-[.6vw] xl:tracking-wide leading-none
+                xl:mt-[4vw]  
+                flex flex-col sm:items-end justify-center`}>
+                <h6 className='smxl:w-[4.2vw] text-left '>Please wait </h6>
+                <h6>a few seconds </h6>
             </div>
         </div>
     </div>
