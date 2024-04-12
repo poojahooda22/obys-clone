b1b9a0ae918451e8134d901ba1972d2a27c14a78
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-12 11:44:32 +0530

    updated app

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,10 @@ function App() {
   
   
   return (
-    <div className='main'>
-        <Loader />
-        <Navbar/>
-        <Landing />
+    <div className='main text-white'>
+      <Loader />
+      <Navbar/>
+      <Landing />
     </div>
   )
 }
