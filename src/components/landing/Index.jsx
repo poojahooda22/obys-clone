9f263f3c8580241b02022019a62d5edb5623afc4
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-13 20:33:43 +0530

    updated index

--- src/components/landing/Index.jsx
+++ src/components/landing/Index.jsx
@@ -8,13 +8,14 @@ function Landing() {
       <Navbar/>
       <div className="w-full h-screen">
         <div className="px-[4vw] py-[16vw]
-          sm:py-[4vw] sm:px-[16vw] 
+          sm:py-[4vw] sm:px-[17vw] space-y-2
           sm:flex sm:flex-row sm:items-start sm:gap-16"
         >
-          <div className="font-[silkSerif] text-[4vw] sm:text-[4vw]">
+          <div className="font-[silkSerif] text-[5vw] sm:text-[2.6vw]">
             <h4>01</h4>
           </div>
-          <div className="sm:text-[7vw] uppercase font-[PlinaReg] leading-[7.2vw] ">
+          <div className="text-[10vw] leading-[10vw] tracking-tighter
+            sm:text-[5.8vw] uppercase font-[PlinaReg] sm:leading-[5.9vw] ">
             <div className="hero">
               <h1>We Design</h1>
             </div>
