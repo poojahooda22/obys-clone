6804f46b5cf3f2e20128d10f22361876edc0816b
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-13 19:05:47 +0530

    updated app

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,37 @@
 
 import './App.css'
-
-
+import { useRef, useEffect } from 'react'
 import Landing from './components/landing/Index';
 import Loader from './components/loader/Index';
+import { LocomotiveScrollProvider } from 'react-locomotive-scroll'
 
 
 function App() {
-  
+  const containerRef = useRef(null)
+  useEffect(() => {
+    // Update the locomotive scroll whenever the location changes
+    if (containerRef.current) {
+      const event = new Event('resize');
+      window.dispatchEvent(event);
+    }
+  }, [location]);
   
   return (
-    <div className='main text-white bg-[#151515]'>
-      <Loader />
-      <Landing />
-    </div>
+    <LocomotiveScrollProvider
+    options={
+      {
+        smooth: true,
+        // ... all available Locomotive Scroll instance options 
+      }
+    }
+    containerRef={containerRef}
+    >
+      <div data-scroll-container ref={containerRef} className='main text-white bg-[#151515]'>
+        <Loader />
+        <Landing />
+      </div>
+    </LocomotiveScrollProvider>
+    
   )
 }
 
