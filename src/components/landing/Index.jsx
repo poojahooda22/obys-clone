426cd4dd5195c94e0e2212ea683329409f85ece5
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-13 23:38:42 +0530

    updated index

--- src/components/landing/Index.jsx
+++ src/components/landing/Index.jsx
@@ -18,7 +18,8 @@ function Landing() {
           sm:flex sm:flex-row sm:items-start sm:gap-16"
         >
           <div 
-            className="font-[silkSerif] text-[5vw] sm:text-[2.6vw]">
+            className="font-[silkSerif] text-[5vw] sm:text-[2.6vw]"
+          >
             <h4>
               01
             </h4>
