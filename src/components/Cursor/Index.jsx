ee03157fb27757490ec752845872dd20653ed94f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-13 23:59:23 +0530

    created

---
+++ src/components/Cursor/Index.jsx
@@ -0,0 +1,11 @@
+import styles from './Style.module.css'
+
+function Cursor() {
+  return (
+    <div>
+        <div className={`${styles.cursor}`}></div> 
+    </div>
+  )
+}
+
+export default Cursor
