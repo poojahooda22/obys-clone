43fbfaf2f1079155ad891b24426109f0cac174f7
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-14 21:22:33 +0530

    updated about

--- src/components/About/Index.jsx
+++ src/components/About/Index.jsx
@@ -2,9 +2,9 @@
 
 function About() {
   return (
-    <div className="page4 relative w-full min-h-[100vh] px-[4vw] py-[6vw]">
+    <div className="page4 relative sm:flex gap-[5vw]  w-full min-h-[100vh] px-[4vw] py-[6vw]">
         <div className="left">
-            <div className="sm:flex sm:ml-[18vw] gap-[3.5vw] ">
+            <div className=" sm:pl-[14vw] ">
                 <div className="font-[silkSerif] text-[4.6vw] sm:text-[2.6vw] 
                     sm:leading-[4vw]"
                 >
@@ -16,20 +16,18 @@ function About() {
 
         <div className="right">
             <div className="text-[8vw] leading-[10vw] tracking-tighter bg
-                sm:text-[6vw] uppercase font-[PlinaReg] sm:leading-[6vw] sm:tracking-normal"
+                sm:text-[6vw]  font-[PlinaReg] sm:leading-[6vw] sm:tracking-normal"
             >
-                <h1>About Obys</h1>
+                <h1 className="uppercase">About Obys</h1>
                 <div className="underline"></div>
+                <h3 className="sm:w-1/2 mt-[4vw] text-[4vw]  sm:[1.5vw] sm:leading-[2vw]">
+                    Our agency is about people who love creating designing and developing wow projects.
+                    In the same time we are boutique agency that is more than the collective. 
+                    We learn and grow, win and celebrate together.
+                </h3>
+
             </div>
         </div>
-      <div className="sm:ml-[24vw] sm:w-1/3 mt-[4vw] text-[2vw] leading-[2.4vw] ">
-        <h3>
-        Our agency is about people who love creating designing and developing wow projects.
-        In the same time we are boutique agency that is more than the collective. 
-        We learn and grow, win and celebrate together.
-        </h3>
-      </div>
-
     </div>
   )
 }
