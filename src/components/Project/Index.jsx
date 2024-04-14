a6077b226755b9e7527777495af4c8001f8fd8d3
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-14 20:54:09 +0530

    updated index

--- src/components/Project/Index.jsx
+++ src/components/Project/Index.jsx
@@ -4,7 +4,7 @@ function Project() {
   return (
     <div>
         <div  className='page3 w-full min-h-[100vh] 
-        bg-zinc-700 relative'>
+            bg-zinc-700 relative'>
             
         </div>
       
