8457226c6d0672648e8a25afd56f8b05cd5a4e6b
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-14 20:51:52 +0530

    updated index

--- src/components/About/Index.jsx
+++ src/components/About/Index.jsx
@@ -9,15 +9,14 @@ function About() {
         >
             <h2>03</h2>
         </div>
-        <div className="text-[8vw] leading-[10vw] tracking-tighter 
-            border-b-2 pb-[1vw] border-[#333]
+        <div className="text-[8vw] leading-[10vw] tracking-tighter bg
             sm:text-[6vw] uppercase font-[PlinaReg] sm:leading-[6vw] sm:tracking-normal"
         >
             <h1>About Obys</h1>
         </div>
       </div>
 
-      <div className="sm:ml-[24vw] w-1/3 mt-[4vw] text-[2vw] leading-[2.4vw] ">
+      <div className="sm:ml-[24vw] sm:w-1/3 mt-[4vw] text-[2vw] leading-[2.4vw] ">
         <h3>
         Our agency is about people who love creating designing and developing wow projects.
         In the same time we are boutique agency that is more than the collective. 
