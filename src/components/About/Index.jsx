0b231eba2dc5247b7ae45e88a351f4ff4f0a0ed2
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-14 21:16:10 +0530

    added index

--- src/components/About/Index.jsx
+++ src/components/About/Index.jsx
@@ -3,20 +3,23 @@
 function About() {
   return (
     <div className="page4 relative w-full min-h-[100vh] px-[4vw] py-[6vw]">
-      <div className="sm:flex sm:ml-[18vw] gap-[3.5vw] ">
-        <div className="font-[silkSerif] text-[4.6vw] sm:text-[2.6vw] 
-            sm:leading-[4vw]"
-        >
-            <h2>03</h2>
+        <div className="left">
+            <div className="sm:flex sm:ml-[18vw] gap-[3.5vw] ">
+                <div className="font-[silkSerif] text-[4.6vw] sm:text-[2.6vw] 
+                    sm:leading-[4vw]"
+                >
+                    <h2>03</h2>
+                </div>
+                <div className="text-[8vw] leading-[10vw] tracking-tighter bg
+                    sm:text-[6vw] uppercase font-[PlinaReg] sm:leading-[6vw] sm:tracking-normal"
+                >
+                    <h1>About Obys</h1>
+                    <div className="underline"></div>
+                </div>
+            </div>
         </div>
-        <div className="text-[8vw] leading-[10vw] tracking-tighter bg
-            sm:text-[6vw] uppercase font-[PlinaReg] sm:leading-[6vw] sm:tracking-normal"
-        >
-            <h1>About Obys</h1>
-            <div className="underline"></div>
-        </div>
-      </div>
 
+        
       <div className="sm:ml-[24vw] sm:w-1/3 mt-[4vw] text-[2vw] leading-[2.4vw] ">
         <h3>
         Our agency is about people who love creating designing and developing wow projects.
