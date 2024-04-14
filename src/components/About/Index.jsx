2f248830e47d50e5b09674bf560d81b7bd18c7cf
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-14 19:26:06 +0530

    updated index

--- src/components/About/Index.jsx
+++ src/components/About/Index.jsx
@@ -2,8 +2,29 @@
 
 function About() {
   return (
-    <div>
-      
+    <div className="page4 relative w-full min-h-[100vh] px-[4vw] py-[6vw]">
+      <div className="sm:flex sm:ml-[18vw] gap-[3.5vw] ">
+        <div className="font-[silkSerif] text-[4.6vw] sm:text-[2.6vw] 
+            sm:leading-[4vw]"
+        >
+            <h2>03</h2>
+        </div>
+        <div className="text-[8vw] leading-[10vw] tracking-tighter 
+            border-b-2 pb-[1vw] border-[#333]
+            sm:text-[6vw] uppercase font-[PlinaReg] sm:leading-[6vw] sm:tracking-normal"
+        >
+            <h1>About Obys</h1>
+        </div>
+      </div>
+
+      <div className="sm:ml-[24vw] w-1/3 mt-[4vw] text-[2vw] leading-[2.4vw] ">
+        <h3>
+        Our agency is about people who love creating designing and developing wow projects.
+        In the same time we are boutique agency that is more than the collective. 
+        We learn and grow, win and celebrate together.
+        </h3>
+      </div>
+
     </div>
   )
 }
