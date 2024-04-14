c20d80890990c2f11431d9c8a11df8c1625d5096
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-14 13:53:51 +0530

    updated index

--- src/components/Video/Index.jsx
+++ src/components/Video/Index.jsx
@@ -2,8 +2,10 @@
 
 function Video() {
   return (
-    <div data-scroll-section className='w-full'>
-      <div>Video</div>
+    <div data-scroll-section className='page2 w-full'>
+      <div id="video-container">
+        <video></video>
+      </div>
     </div>
   )
 }
