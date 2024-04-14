4990de5353c38ec02df59569139f4be6ae3def4a
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-14 17:32:27 +0530

    created index

---
+++ src/components/Project/Index.jsx
@@ -0,0 +1,14 @@
+
+
+function Project() {
+  return (
+    <div>
+        <div  className='page3 w-full min-h-full bg-zinc-700'>
+            
+        </div>
+      
+    </div>
+  )
+}
+
+export default Project
