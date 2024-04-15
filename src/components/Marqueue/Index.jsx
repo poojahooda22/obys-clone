1ace89aa1366cdb89f40deb3f265b3103231632f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-15 17:13:44 +0530

    created index

---
+++ src/components/Marqueue/Index.jsx
@@ -0,0 +1,20 @@
+
+function Marqueue() {
+  return (
+    <div>
+        <div className="page5 w-full h-[100vh] px-[4vw] sm:px-[3vw] mt-[4vw] ">
+            {/* <div className="sm:flex">
+                <div className="w-[32%]"></div>
+                <div className="w-full border-t-[.1vw] py-[5vw] sm:py-[1.3vw]">
+                    <p className="font-[PlinaReg] text-[3.5vw] sm:text-[.8vw]">We work with</p>
+                </div>
+            </div> */}
+
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default Marqueue
