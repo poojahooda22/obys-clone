69724a7f46e3360ee24736670f32089abce00c4e
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-15 22:15:33 +0530

    updated div

--- src/components/Marqueue/Index.jsx
+++ src/components/Marqueue/Index.jsx
@@ -8,7 +8,7 @@ function Marqueue() {
     <div>
         <div className="page5 w-full h-[100vh] px-[4vw] sm:px-[3vw] relative">
             {/* <div className="sm:flex">
-                <div className="w-[32%]"></div>
+                <div className="w-[32%]">
             </div> */}
 
             {/* Repeating row */}
