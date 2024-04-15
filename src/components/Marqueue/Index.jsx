9d2ee24d5922111677d58a3a82c00d32ff10b34e
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-15 22:28:37 +0530

    added

--- src/components/Marqueue/Index.jsx
+++ src/components/Marqueue/Index.jsx
@@ -40,7 +40,9 @@ function Marqueue() {
                 <h1 className={`${styles.elemh1} flex items-center gap-[3vw] font-[silkSerif] `}>real estate
                   <div className={`${styles.dash} w-[5vw] h-[.5vw] border-[1px] border-[#fff]`}></div>
                 </h1>
-                <h1 className={`${styles.elemh1} flex items-center gap-[3vw]`}>architecture <div className={`${styles.dash} w-[5vw] h-[.5vw] border-[1px] border-[#fff]`}></div></h1>
+                <h1 className={`${styles.elemh1} flex items-center gap-[3vw]`}>architecture 
+                  <div className={`${styles.dash} w-[5vw] h-[.5vw] border-[1px] border-[#fff]`}></div>
+                </h1>
                 <h1 className={`${styles.elemh1} flex items-center gap-[3vw] font-[silkSerif] `}>beauty <div className={`${styles.dash} w-[5vw] h-[.5vw] border-[1px] border-[#fff]`}></div></h1>
                 <h1 className={`${styles.elemh1} flex items-center gap-[3vw]`}>real estate <div className={`${styles.dash} w-[5vw] h-[.5vw] border-[1px] border-[#fff]`}></div></h1>
                 <h1 className={`${styles.elemh1} flex items-center gap-[3vw] font-[silkSerif] `}>architecture <div className={`${styles.dash} w-[5vw] h-[.5vw] border-[1px] border-[#fff]`}></div></h1>
