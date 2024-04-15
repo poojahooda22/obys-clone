e8fac972e8ac571652475b8f2ed247cca0af680c
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-15 22:01:03 +0530

    updated index

--- src/components/Marqueue/Index.jsx
+++ src/components/Marqueue/Index.jsx
@@ -11,7 +11,7 @@ function Marqueue() {
                 <div className="w-[32%]"></div>
                 <div className="w-full border-t-[.1vw] py-[5vw] sm:py-[1.3vw]">
                     <p className="font-[PlinaReg] text-[3.5vw] sm:text-[.8vw]">We work with</p>
-                </div>
+                
             </div> */}
 
             <div>
