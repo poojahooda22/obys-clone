8e78a0440f66f028ae09ef3efb498909a50ab0ad
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-15 22:07:44 +0530

    updated index

--- src/components/Marqueue/Index.jsx
+++ src/components/Marqueue/Index.jsx
@@ -10,7 +10,7 @@ function Marqueue() {
             {/* <div className="sm:flex">
                 <div className="w-[32%]"></div>
                 <div className="w-full border-t-[.1vw] py-[5vw] sm:py-[1.3vw]">
-                    <p className="font-[PlinaReg] 
+                    <p className="font-
             </div> */}
 
             {/* Repeating row */}
