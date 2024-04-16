b3a5184eb28c54c74d4d7bac1047732d65a44e7f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-16 15:03:22 +0530

    updated video

--- src/components/Video/Index.jsx
+++ src/components/Video/Index.jsx
@@ -1,33 +1,99 @@
-import { RiPlayFill } from "@remixicon/react";
+import { RiPlayFill, RiPauseMiniLine } from "@remixicon/react";
+import gsap from "gsap";
+import { useEffect } from "react";
+
 
 function Video() {
+
+  useEffect(() => {
+    var videoContainer = document.querySelector("#video-container")
+    var video = document.querySelector("#video-container video")
+    videoContainer.addEventListener("mouseenter", function() {
+     videoContainer.addEventListener("mousemove", function(dets) {
+
+        videoContainer.style.cursor = "none"
+        gsap.to(".cursor", {
+          opacity: 0,
+        })
+        gsap.to("#video-cursor", {
+          left: dets.x,
+          y: dets.y 
+        })
+     })
+    })
+    videoContainer.addEventListener("mouseleave", function() {
+      gsap.to(".cursor", {
+        opacity: 1,
+      })
+      gsap.to("#video-cursor", {
+        left: "64%",
+        y: "10%" 
+      })
+      
+    })
+
+    var flag = 0;
+    videoContainer.addEventListener("click", function() {
+      if(flag == 0) {
+        video.play()
+        video.style.opacity = 1
+        
+        gsap.to("#video-cursor", {
+          scale: 0.5
+        })
+        gsap.to(".playIcon", {
+          opacity: 0
+        })
+        gsap.to(".pauseIcon", {
+          opacity: 1
+        })
+        flag = 1
+      } else {
+        video.pause()
+        video.style.opacity = 0
+        
+        gsap.to("#video-cursor", {
+          scale: 1
+        })
+        gsap.to(".playIcon", {
+          opacity: 1
+        })
+        gsap.to(".pauseIcon", {
+          opacity: 0
+        })
+        flag = 0
+      }
+    })
+  })
+
+
   return (
     <div  className='page2 w-full py-[6vw] relative '>
       <div id="video-container" 
-        className="h-[58vh]
-        sm:h-[74vh] sm:w-[80vw] sm:left-[26%] relative"
+        className={` w-full h-[58vh] bg-custom-image bg-cover bg-center
+        sm:h-[74vh] sm:w-[80vw] sm:left-[26%] relative`}
       >
         <div 
           id="video-cursor"
           className="bg-[#ffa63d] absolute top-[10%] left-[20%] sm:top-[-10%] sm:left-[64%] w-[22vw] h-[22vw]
-          sm:h-[6.4vw] sm:w-[6.4vw] rounded-full z-[999]"
+          sm:h-[6.4vw] sm:w-[6.4vw] rounded-full z-[9]"
         >  
           <RiPlayFill
             size={32}
             color="white"
-            className="absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2" 
+            className={`playIcon absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2`} 
+          /> 
+          <RiPauseMiniLine
+            size={32}
+            color="white"
+            className={`pauseIcon absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 opacity-0`} 
           /> 
         </div>
-        <img 
-          src="https://obys.agency/wp-content/uploads/2022/11/Showreel-2022-preview-1.jpg"
-          className="w-full h-full object-cover absolute top-0 left-0"
-        ></img>
         <video 
-          autoPlay
           loop
           muted
           src="https://obys.agency/wp-content/uploads/2022/11/Obys-Showreel-2022.mp4"
-          className="w-full h-full object-cover"
+          className="w-full h-full object-cover opacity-0"
         >
         </video>
         
