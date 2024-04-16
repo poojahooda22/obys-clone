bd367dbed0af3234c05869200fb5e766ed4a7369
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-16 21:08:56 +0530

    updated index

--- src/components/Project/Index.jsx
+++ src/components/Project/Index.jsx
@@ -4,9 +4,33 @@ function Project() {
   return (
     <div>
         <div  
-            className='page3 w-full min-h-[100vh] 
-            bg-zinc-700 relative'
+          className='page3 relative sm:flex gap-[5vw]  w-full min-h-[100vh]  px-[4vw] py-[6vw]
+          sm:px-[4vw]'
         >
+          <div className="left">
+            <div className=" sm:pl-[14vw] ">
+                <div className="font-[silkSerif] text-[4.6vw] 
+                    sm:text-[2.6vw] sm:leading-[4vw]"
+                >
+                    <h2>03</h2>
+                </div>             
+            </div>
+          </div>
+          <div className="right">
+            <div className="aboutHeading overflow-hidden">
+                    <h1 className="text-[8vw] leading-[10vw] tracking-tighter
+                        sm:text-[6vw] font-[PlinaReg] sm:leading-[6vw] sm:tracking-normal
+                        uppercase"
+                    >
+                        About Obys
+                    </h1>
+            </div>
+            <div 
+                className="underline mt-[6vw] mb-[11vw] w-0 h-[.25vw] 
+                sm:h-[.01vw] sm:mt-[3.8vw] sm:mb-[5vw] bg-white"
+            ></div>
+          </div>
+          
             
         </div>
       
