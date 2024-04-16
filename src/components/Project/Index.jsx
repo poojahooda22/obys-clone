8c4a46f563bb37d1d3b618738e80da3d3a847462
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-16 21:18:34 +0530

    updated index

--- src/components/Project/Index.jsx
+++ src/components/Project/Index.jsx
@@ -2,37 +2,42 @@
 
 function Project() {
   return (
-    <div>
+    <div className="page3 relative w-full min-h-[100vh]  px-[4vw] py-[6vw]
+    sm:px-[4vw]">
         <div  
-          className='page3 relative sm:flex gap-[5vw]  w-full min-h-[100vh]  px-[4vw] py-[6vw]
-          sm:px-[4vw]'
+          className=' sm:flex gap-[5vw] '
         >
           <div className="left">
             <div className=" sm:pl-[14vw] ">
                 <div className="font-[silkSerif] text-[4.6vw] 
                     sm:text-[2.6vw] sm:leading-[4vw]"
                 >
-                    <h2>03</h2>
+                    <h2>02</h2>
                 </div>             
             </div>
           </div>
-          <div className="right">
+          <div className="w-full right">
             <div className="aboutHeading overflow-hidden">
               <h1 className="text-[8vw] leading-[10vw] tracking-tighter
                 sm:text-[6vw] font-[PlinaReg] sm:leading-[6vw] sm:tracking-normal
                 uppercase"
               >
-                About Obys
+                our Projects
               </h1>
             </div>
             <div 
-                className="underline mt-[6vw] mb-[11vw] w-0 h-[.25vw] 
+                className="underline mt-[6vw] mb-[11vw] w-full h-[.25vw] 
                 sm:h-[.01vw] sm:mt-[3.8vw] sm:mb-[5vw] bg-white"
             ></div>
-          </div>
-          
-            
+          </div>  
         </div>
+
+        <div className="mt-[2vw] w-full sm:flex items-start gap-10 bg-green-400">
+              <div className="first">Olga Prudka</div>
+              <div className="second">Aim</div>
+              <div className="third">Button</div>
+
+            </div>
       
     </div>
   )
