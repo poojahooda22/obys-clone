6ec031dc85c76172a2ff86b72ddfc86cc17a5ea9
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-17 22:17:49 +0530

    updated index

--- src/components/Marqueue/Index.jsx
+++ src/components/Marqueue/Index.jsx
@@ -125,7 +125,8 @@ function Marqueue() {
                 <h1 className={`${styles.elemh1} flex items-center gap-[3vw] font-[silkSerif] `}>hotels 
                   <div className={`${styles.dash} w-[8vw] h-[1vw] sm:w-[5vw] sm:h-[.5vw] border-[1px] border-[#fff]`}></div>
                 </h1>
-                <h1 className={`${styles.elemh1} flex items-center gap-[3vw]`}>music 
+                <h1 className={`${styles.elemh1} flex items-center gap-[3vw]`}>
+                  music 
                   <div className={`${styles.dash} w-[8vw] h-[1vw] sm:w-[5vw] sm:h-[.5vw] border-[1px] border-[#fff]`}></div>
                 </h1>
                 <h1 className={`${styles.elemh1} flex items-center gap-[3vw] font-[silkSerif] `}>automotive
