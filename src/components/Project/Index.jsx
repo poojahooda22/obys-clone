d0fe307ffaefa809ce34bc8d86327ed1c0e04636
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-17 09:33:28 +0530

    updated index

--- src/components/Project/Index.jsx
+++ src/components/Project/Index.jsx
@@ -1,20 +1,55 @@
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import styles from './Style.module.css'
-// import hoverEffect from 'hover-effect';
 
 function Project() {
 
-  let wrappers = useRef();
+  const path = useRef(null);
+  let progress = 0;
 
-  // useEffect(() => {
-  //   new hoverEffect({
-  //     parent: wrappers.current,
-  //     intensity: 0.5,
-  //     image1: '/src/assets/images/1.jpg',
-  //     image2: '/src/assets/images/2.jpg',
-  //     displacementImage: '/src/assets/images/12.jpg'
-  //   });
-  // })
+  useEffect(() => {
+    setPath(progress);
+  }, []);
+
+  const setPath = (progress) => {
+    const {innerWidth} = window;
+    const width = innerWidth * .7;
+    path.current.setAttributeNS("", "d", `M0 50 Q${width / 2} ${50 + progress}, ${width} 50`)
+  }
+
+  const manageMouseMove = (e) => {
+    const {movementY} = e;
+    progress += movementY;
+    setPath(progress);
+  }
+
+  const manageMouseLeave = () => {
+    animateOut();
+  }
+
+  const lerp = (x,y,a) => x * (1-a) + y * a;
+  let time = Math.PI / 2;
+
+  const animateOut = () => {
+    const newProgress = progress * Math.sin(time);
+    time += 0.2;
+    setPath(newProgress);
+    progress = lerp(progress, 0, 0.025);
+
+    if(Math.abs(progress) > 0.75) {
+      window.requestAnimationFrame(animateOut);
+    }
+    else {
+      resetAnimation();
+    }
+  }
+
+  const resetAnimation = () => {
+    time = Math.PI / 2;
+    progress = 0;
+
+  }
+
+
 
 
   return (
@@ -41,10 +76,25 @@ function Project() {
                 our Projects
               </h1>
             </div>
+
+            {/*Line animation */}
             <div 
-                className="underline mt-[6vw] mb-[11vw] w-full h-[.25vw] 
-                sm:h-[.01vw] sm:mt-[3.8vw] sm:mb-[5vw] bg-white"
-            ></div>
+              className={`line ${styles.line} relative mt-[6vw] mb-[11vw] w-full h-[.25vw] 
+              sm:h-[.01vw] sm:mt-[3.8vw] sm:mb-[5vw] `}
+            >
+              <div 
+                className={`box ${styles.box}`} 
+                onMouseMove={manageMouseMove}
+                onMouseLeave={manageMouseLeave}
+              >
+              </div>
+              <svg className='w-full h-[100px] absolute -top-[50px]'>
+                <path ref={path} className={`path1 ${styles.path1}`} ></path>
+              </svg>
+            </div>
+
+
+
           </div>  
         </div>
 
@@ -55,7 +105,6 @@ function Project() {
             </div>
             <div id="imageContainer"
               className={`imageContainer ${styles.imageContainer} w-[22vw] relative overflow-hidden `}
-              ref={wrappers}
             >  
             </div>
             <div>
