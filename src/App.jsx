94de8b4f66a2398f223fb32c763230f0389a2205
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-17 10:57:53 +0530

    updated app

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,7 @@ function App() {
           <Landing />
           {/* video */}
           <Video />
-          <Project/>
+          {/* <Project/> */}
           <About/>
           <Marqueue/>
           <Footer/>
