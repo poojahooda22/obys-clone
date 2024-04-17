842b7baf6b97dc81d2e71e8007477b47c1364fd7
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-17 22:09:55 +0530

    updated index

--- src/components/Marqueue/Index.jsx
+++ src/components/Marqueue/Index.jsx
@@ -60,7 +60,8 @@ function Marqueue() {
                   <div className={`${styles.dash} w-[8vw] h-[1vw] sm:w-[5vw] sm:h-[.5vw] border-[1px] border-[#fff]`}></div>
                 </h1>
               </div>
-              <div className={`elem whitespace-nowrap text-[9.6vw] leading-[12vw] sm:text-[7vw] sm:leading-[7.6vw] 
+              <div 
+              className={`elem whitespace-nowrap text-[9.6vw] leading-[12vw] sm:text-[7vw] sm:leading-[7.6vw] 
                 flex items-center uppercase font-[PlinaReg] ${styles.elem}`}
               >
                 <h1 className={`${styles.elemh1} flex items-center gap-[3vw]`}>partners
