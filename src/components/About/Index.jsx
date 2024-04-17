a9389f997aaac7d23c67601bb8bba598c8f0d648
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-17 09:32:43 +0530

    updated about

--- src/components/About/Index.jsx
+++ src/components/About/Index.jsx
@@ -1,20 +1,64 @@
 import { useGSAP } from "@gsap/react"
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useEffect, useRef } from 'react';
+import styles from './Style.module.css'
+
+
+
 gsap.registerPlugin(ScrollTrigger);
 
 function About() {
 
+    
+  const path = useRef(null);
+  let progress = 0;
+
+  useEffect(() => {
+    setPath(progress);
+  }, []);
+
+  const setPath = (progress) => {
+    const {innerWidth} = window;
+    const width = innerWidth * .7;
+    path.current.setAttributeNS("", "d", `M0 50 Q${width / 2} ${50 + progress}, ${width} 50`)
+  }
+
+  const manageMouseMove = (e) => {
+    const {movementY} = e;
+    progress += movementY;
+    setPath(progress);
+  }
+
+  const manageMouseLeave = () => {
+    animateOut();
+  }
+
+  const lerp = (x,y,a) => x * (1-a) + y * a;
+  let time = Math.PI / 2;
+
+  const animateOut = () => {
+    const newProgress = progress * Math.sin(time);
+    time += 0.2;
+    setPath(newProgress);
+    progress = lerp(progress, 0, 0.025);
+
+    if(Math.abs(progress) > 0.75) {
+      window.requestAnimationFrame(animateOut);
+    }
+    else {
+      resetAnimation();
+    }
+  }
+
+  const resetAnimation = () => {
+    time = Math.PI / 2;
+    progress = 0;
+
+  }
+
 
     useGSAP(() => {
-    // const tl = gsap.timeline({
-    //     scrollTrigger: {
-    //         trigger: '.right',
-    //         start: "top 100%",
-    //         markers: true,
-    //         scrub: .5,
-    //     },
-    // });
     gsap.from('.aboutHeading h1', {
         y: 120,
     })
@@ -37,6 +81,7 @@ function About() {
     })
 
     })
+
   return (
     <div className="page4 relative sm:flex gap-[5vw]  w-full  px-[4vw] py-[6vw]
         sm:px-[4vw]
@@ -60,10 +105,26 @@ function About() {
                         About Obys
                     </h1>
                 </div>
-                <div 
+                {/* <div 
                     className="underline mt-[6vw] mb-[11vw] w-0 h-[.25vw] 
                     sm:h-[.01vw] sm:mt-[3.8vw] sm:mb-[5vw] bg-white"
-                ></div>
+                ></div> */}
+
+                {/*Line animation */}
+                <div 
+                className={`line ${styles.line} relative mt-[6vw] mb-[11vw] w-full h-[.25vw] 
+                sm:h-[.01vw] sm:mt-[4vw] sm:mb-[5vw] `}
+                >
+                    <div 
+                        className={`box ${styles.box}`} 
+                        onMouseMove={manageMouseMove}
+                        onMouseLeave={manageMouseLeave}
+                    >
+                    </div>
+                    <svg className='w-full h-[100px] absolute -top-[50px]'>
+                        <path ref={path} className={`path1 ${styles.path1}`} ></path>
+                    </svg>
+                </div>
                 <div 
                     className="sm:w-2/4 text-[5.5vw] tracking-normal leading-[6vw] font-[PlinaReg]
                     sm:text-[1.8vw] sm:leading-[2.2vw]"
@@ -136,9 +197,24 @@ function About() {
                         </div>
                     </div>        
                 </div>
+                {/* Line animation */}
+                {/* <div 
+                className={`line ${styles.line} relative mt-[6vw] mb-[11vw] w-full h-[.25vw] 
+                sm:h-[.01vw] sm:mt-[18vw] sm:mb-[1.2vw] `}
+                >
+                    <div 
+                        className={`box ${styles.box}`} 
+                        onMouseMove={manageMouseMove}
+                        onMouseLeave={manageMouseLeave}
+                    >
+                    </div>
+                    <svg className='w-full h-[100px] absolute -top-[50px]'>
+                        <path ref={path} className={`path1 ${styles.path1}`} ></path>
+                    </svg>
+                </div> */}
                 <div 
-                    className="underline2 mt-[18vw] mb-[4vw] w-0 h-[.25vw] 
-                    sm:h-[.01vw] sm:mt-[20vw] sm:mb-[1.2vw] bg-white"
+                    className="underline mt-[16vw] mb-[9vw] w-full h-[.25vw] 
+                    sm:h-[.01vw] sm:mt-[18vw] sm:mb-[1vw] bg-white"
                 ></div>
                 <p className="font-[PlinaReg] text-[3.5vw] sm:text-[.8vw]">We work with</p>
             </div>    
