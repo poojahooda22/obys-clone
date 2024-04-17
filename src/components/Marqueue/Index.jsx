301dc16f802a3f5d4d8d5710427e968d4da8b936
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-17 22:05:29 +0530

    updated index

--- src/components/Marqueue/Index.jsx
+++ src/components/Marqueue/Index.jsx
@@ -9,7 +9,8 @@ function Marqueue() {
         <div className="page5 w-full  px-[4vw] mt-[3vw] sm:px-[3vw] relative">
             {/* Repeating row */}
             <div>
-              <div className={`elem whitespace-nowrap text-[9.6vw] leading-[12vw] sm:text-[7vw] sm:leading-[7.6vw] 
+              <div 
+              className={`elem whitespace-nowrap text-[9.6vw] leading-[12vw] sm:text-[7vw] sm:leading-[7.6vw] 
                 flex items-center uppercase font-[PlinaReg] ${styles.elem}`}
               >
                 <h1 className={`${styles.elemh1} flex items-center gap-[3vw]`}>Sport 
