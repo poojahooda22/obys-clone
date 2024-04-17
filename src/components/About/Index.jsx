e022b0f9678a9d26a71223bb3cec09396c6177ef
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-17 10:52:42 +0530

    updated index

--- src/components/About/Index.jsx
+++ src/components/About/Index.jsx
@@ -1,61 +1,12 @@
 import { useGSAP } from "@gsap/react"
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { useEffect, useRef } from 'react';
-import styles from './Style.module.css'
-
+import UnderLine from '../Underline/Index'
 
 
 gsap.registerPlugin(ScrollTrigger);
 
 function About() {
-
-    
-  const path = useRef(null);
-  let progress = 0;
-
-  useEffect(() => {
-    setPath(progress);
-  }, []);
-
-  const setPath = (progress) => {
-    const {innerWidth} = window;
-    const width = innerWidth * .7;
-    path.current.setAttributeNS("", "d", `M0 50 Q${width / 2} ${50 + progress}, ${width} 50`)
-  }
-
-  const manageMouseMove = (e) => {
-    const {movementY} = e;
-    progress += movementY;
-    setPath(progress);
-  }
-
-  const manageMouseLeave = () => {
-    animateOut();
-  }
-
-  const lerp = (x,y,a) => x * (1-a) + y * a;
-  let time = Math.PI / 2;
-
-  const animateOut = () => {
-    const newProgress = progress * Math.sin(time);
-    time += 0.2;
-    setPath(newProgress);
-    progress = lerp(progress, 0, 0.025);
-
-    if(Math.abs(progress) > 0.75) {
-      window.requestAnimationFrame(animateOut);
-    }
-    else {
-      resetAnimation();
-    }
-  }
-
-  const resetAnimation = () => {
-    time = Math.PI / 2;
-    progress = 0;
-
-  }
 
 
     useGSAP(() => {
@@ -111,7 +62,7 @@ function About() {
                 ></div> */}
 
                 {/*Line animation */}
-                <div 
+                {/* <div 
                 className={`line ${styles.line} relative mt-[6vw] mb-[11vw] w-full h-[.25vw] 
                 sm:h-[.01vw] sm:mt-[4vw] sm:mb-[5vw] `}
                 >
@@ -124,7 +75,8 @@ function About() {
                     <svg className='w-full h-[100px] absolute -top-[50px]'>
                         <path ref={path} className={`path1 ${styles.path1}`} ></path>
                     </svg>
-                </div>
+                </div> */}
+                <UnderLine marginBottom='4vw' marginTop='4vw' />
                 <div 
                     className="sm:w-2/4 text-[5.5vw] tracking-normal leading-[6vw] font-[PlinaReg]
                     sm:text-[1.8vw] sm:leading-[2.2vw]"
@@ -198,24 +150,11 @@ function About() {
                     </div>        
                 </div>
                 {/* Line animation */}
+                <UnderLine  marginBottom='1vw' marginTop='18vw'  />
                 {/* <div 
-                className={`line ${styles.line} relative mt-[6vw] mb-[11vw] w-full h-[.25vw] 
-                sm:h-[.01vw] sm:mt-[18vw] sm:mb-[1.2vw] `}
-                >
-                    <div 
-                        className={`box ${styles.box}`} 
-                        onMouseMove={manageMouseMove}
-                        onMouseLeave={manageMouseLeave}
-                    >
-                    </div>
-                    <svg className='w-full h-[100px] absolute -top-[50px]'>
-                        <path ref={path} className={`path1 ${styles.path1}`} ></path>
-                    </svg>
-                </div> */}
-                <div 
-                    className="underline mt-[16vw] mb-[9vw] w-full h-[.25vw] 
+                    className="underline mt-[16vw] mb-[5vw] w-full h-[.25vw] 
                     sm:h-[.01vw] sm:mt-[18vw] sm:mb-[1vw] bg-white"
-                ></div>
+                ></div> */}
                 <p className="font-[PlinaReg] text-[3.5vw] sm:text-[.8vw]">We work with</p>
             </div>    
         </div>
