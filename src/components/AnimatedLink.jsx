ed273dd7d80f7b00236f9edf4922037bcd871d35
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-18 10:09:57 +0530

    created animated link

---
+++ src/components/AnimatedLink.jsx
@@ -0,0 +1,97 @@
+import PropTypes from 'prop-types';
+import { motion } from 'framer-motion';
+import { useState } from 'react';
+
+
+export default function AnimatedLink({title}) {
+    const [isHovered, setisHovered] = useState(false);
+  return (
+    <motion.div 
+    onMouseEnter={() => setisHovered(true)}
+    onMouseLeave={() => setisHovered(false)}
+        className="cursor-pointer relative overflow-hidden"
+    >
+        <AnimatedWord title={title} animation={letterAnimation} isHovered={isHovered} />
+        <div className='absolute top-0'>
+            <AnimatedWord title={title} animation={letterAnimationTwo} isHovered={isHovered} />
+        </div>
+        
+    </motion.div>
+  )
+}
+
+const titleAnimation = {
+    rest: {
+        transition: {
+            staggerChildren: 0.025,
+        }
+    },
+    hover: {
+        transition: {
+            staggerChildren: 0.025,
+        }
+    }
+}
+
+const letterAnimation = {
+    rest: {
+        y: 0,
+    },
+    hover: {
+        y: -35,
+        transition: {
+            duration: 0.35,
+            ease: [.3,.86,.36,.95],
+            type: "tween",
+        },
+    },
+}
+
+
+const letterAnimationTwo = {
+    rest: {
+        y: 35,
+    },
+    hover: {
+        y: 0,
+        transition: {
+            duration: 0.35,
+            ease: [.3,.86,.36,.95],
+            type: "tween",
+        },
+    },
+}
+
+
+AnimatedLink.propTypes = {
+    title: PropTypes.string.isRequired,  
+};
+
+const AnimatedWord = ({title, animation, isHovered}) => {
+    return (
+        <motion.span 
+            variants={titleAnimation}
+            initial="rest"
+            animate={isHovered ? "hover" : "rest"}
+            className="relative whitespace-nowrap">
+            {title.split("").map((character, i) => 
+                character === " " ? (
+                    <span  key={i}> &nbsp;</span> 
+                ) : (
+                    <motion.span key={i}
+                    variants={animation}
+                        className="relative inline-block whitespace-nowrap"
+                    >
+                        {character}
+                    </motion.span>
+                )
+        )}
+        </motion.span>
+    )
+} 
+
+AnimatedWord.propTypes = {
+    title: PropTypes.string.isRequired,
+    animation: PropTypes.object.isRequired,
+    isHovered: PropTypes.bool.isRequired,  
+};
