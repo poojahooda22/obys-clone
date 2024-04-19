b9d4cbb50193b402d5f686e769d93b9d4bbd8d93
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-19 19:37:09 +0530

    button

--- src/components/CenterButton.jsx
+++ src/components/CenterButton.jsx
@@ -9,7 +9,12 @@ function CenterButton() {
         className="mt-[4vw] mb-[5vw] flex items-center justify-center bg-red-300"
     >
         <div className="relative">
-            <motion.div className="bg-[#ffa63d] w-[20vw] h-[20vw] rounded-full  relative">
+            <motion.div 
+                whileHover={{ scale: .95, }}
+                whileTap={{ scale: 1.1 }}
+                transition={{ease: [0.76, 0, 0.24, 1]}}
+                className="bg-[#ffa63d] w-[20vw] h-[20vw] rounded-full  relative"
+            >
                <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
                     <h2 className='font-[PlinaReg] text-[.9vw]'>All Projects<span className="ml-[.3vw] font-[silkSerif]">(12)</span></h2>
                </div>
