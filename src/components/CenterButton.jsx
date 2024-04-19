a9d21e52e56edf98907c94a1b870fe5a2186e79d
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-19 20:28:00 +0530

    updated button

--- src/components/CenterButton.jsx
+++ src/components/CenterButton.jsx
@@ -5,15 +5,14 @@ import { motion } from 'framer-motion'
 function CenterButton() {
   return (
     <motion.div
-     
-        className="mt-[4vw] mb-[5vw] flex items-center justify-center bg-red-300"
+        className="mt-[4vw] mb-[5vw] flex items-center justify-center "
     >
         <div className="relative">
             <motion.div 
-                whileHover={{ scale: .95, }}
-                whileTap={{ scale: 1.1 }}
+                whileHover={{ scale: .85, }}
+                whileTap={{ scale: 1.3 }}
                 transition={{ease: [0.76, 0, 0.24, 1]}}
-                className="bg-[#ffa63d] w-[20vw] h-[20vw] rounded-full  relative"
+                className="bg-[#ffa63d] w-[22vw] h-[22vw] rounded-full  relative"
             >
                <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
                     <h2 className='font-[PlinaReg] text-[.9vw]'>All Projects<span className="ml-[.3vw] font-[silkSerif]">(12)</span></h2>
