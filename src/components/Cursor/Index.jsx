b2cc0372f0206c35c5b2c2b80099f94bdfafbf56
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-22 20:15:04 +0530

    added

--- src/components/Cursor/Index.jsx
+++ src/components/Cursor/Index.jsx
@@ -8,7 +8,7 @@ function Cursor() {
   useEffect(() => {
         // document.addEventListener('mousemove', function(dets) {
         //    gsap.to('.cursor', {
-        //         left:
+        //         le
         // })
 
     const circleElement = document.querySelector('.cursor');
